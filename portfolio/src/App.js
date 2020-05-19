12e00cbba68f6a047ff87f56b959c5a8a6b0e279
mayahopper <mhopper@uoregon.edu>
2020-05-19 15:32:06 -0700

    added navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import Projects from './Projects.js';
 import About from './About.js';
+import './Navbar.css';
 
 function App() {
   return (
@@ -13,12 +14,15 @@ function App() {
       <Route path='/Projects' component={Projects} />
       <Route path='/About' component={About} />
 
+      <h1>MAYA HOPPER </h1>
        <div className="navigation">
      
         <div className="navigation-sub">
           {/*Links */}
-          <Link to="/Projects" className="item">Projects</Link>
-          <Link to ="/About" className="item">About</Link>
+          <Link to="/Projects" className="item">PROJECTS</Link>
+          <Link to ="/About" className="item">ABOUT</Link>
+          <Link to ="/something" className="item">SOMETHINGV</Link>
+          <img src =""></img> 
         </div>
       </div>
 
