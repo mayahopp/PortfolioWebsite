9c2a1329048fd59240f01d1b8f0c64fcba44b5c7
mayahopper <mhopper@uoregon.edu>
2020-10-13 21:36:39 -0700

    Updates

---
+++ precache-manifest.12b5797b8444d1cbe2a758a9d0cd4265.js
@@ -0,0 +1,466 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7800b8e6db93577b44b995cd70a4a12f",
+    "url": "/PortfolioWebsite/index.html"
+  },
+  {
+    "revision": "f50c9e68241c456b29af",
+    "url": "/PortfolioWebsite/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "98cdc711ec333ca48f3f",
+    "url": "/PortfolioWebsite/static/css/main.63965fe6.chunk.css"
+  },
+  {
+    "revision": "f50c9e68241c456b29af",
+    "url": "/PortfolioWebsite/static/js/2.0509d4be.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/PortfolioWebsite/static/js/2.0509d4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cdc711ec333ca48f3f",
+    "url": "/PortfolioWebsite/static/js/main.3dafe51a.chunk.js"
+  },
+  {
+    "revision": "cb61986a5b9b9180a265",
+    "url": "/PortfolioWebsite/static/js/runtime-main.799258d4.js"
+  },
+  {
+    "revision": "9c7e0845838b802b5f7807a0e438287a",
+    "url": "/PortfolioWebsite/static/media/10collumn.9c7e0845.jpg"
+  },
+  {
+    "revision": "e88092dab198d04aad90ef8005426489",
+    "url": "/PortfolioWebsite/static/media/360d1.e88092da.jpg"
+  },
+  {
+    "revision": "2138052bde23a44a0e14a06ea1e5260d",
+    "url": "/PortfolioWebsite/static/media/360d3.2138052b.jpg"
+  },
+  {
+    "revision": "00340ae3a58b017b543c2f8a8b6b581f",
+    "url": "/PortfolioWebsite/static/media/360d4.00340ae3.jpg"
+  },
+  {
+    "revision": "802b98cc38a633536936719c835ee6a4",
+    "url": "/PortfolioWebsite/static/media/360hair1.802b98cc.jpg"
+  },
+  {
+    "revision": "37b9e0a4d5f93e95d3e4f862d954df67",
+    "url": "/PortfolioWebsite/static/media/3collumn.37b9e0a4.jpg"
+  },
+  {
+    "revision": "0bf32a58180e3a130c13ef67d069ad65",
+    "url": "/PortfolioWebsite/static/media/4collumn.0bf32a58.jpg"
+  },
+  {
+    "revision": "61297f636ea3afee63e38171b32d442f",
+    "url": "/PortfolioWebsite/static/media/5collumn.61297f63.jpg"
+  },
+  {
+    "revision": "9879c2c6ac47b90894069feb695db71d",
+    "url": "/PortfolioWebsite/static/media/6collumn.9879c2c6.jpg"
+  },
+  {
+    "revision": "ad580483020007e2a530440cb0a197fd",
+    "url": "/PortfolioWebsite/static/media/7collumn.ad580483.jpg"
+  },
+  {
+    "revision": "7a6bcc7a52c6412b79d31b590ef3079f",
+    "url": "/PortfolioWebsite/static/media/8collumn.7a6bcc7a.jpg"
+  },
+  {
+    "revision": "b5323f713514c8298d1c31da14a67efa",
+    "url": "/PortfolioWebsite/static/media/9collumn.b5323f71.jpg"
+  },
+  {
+    "revision": "82f3897c0eca66edb896d6fc0649cb37",
+    "url": "/PortfolioWebsite/static/media/Font.82f3897c.jpg"
+  },
+  {
+    "revision": "3cbe9257e888255cb2dad0d8a80a7759",
+    "url": "/PortfolioWebsite/static/media/NanumGothic-Regular.3cbe9257.ttf"
+  },
+  {
+    "revision": "f8d2ce1a621accfb08357e6eb58d9827",
+    "url": "/PortfolioWebsite/static/media/NomadsJersey.f8d2ce1a.jpg"
+  },
+  {
+    "revision": "ff7a7f8db07e285ccc5facd6dbfeeafb",
+    "url": "/PortfolioWebsite/static/media/Poster1.ff7a7f8d.png"
+  },
+  {
+    "revision": "7e27735f5cf0f9966cb60b7e05d6a096",
+    "url": "/PortfolioWebsite/static/media/Poster2.7e27735f.png"
+  },
+  {
+    "revision": "fb987593d286bb6bd93d3de8457c5eb4",
+    "url": "/PortfolioWebsite/static/media/Rajdhani-Regular.fb987593.ttf"
+  },
+  {
+    "revision": "ec298d7d3f6855c11f31b8b551740ffd",
+    "url": "/PortfolioWebsite/static/media/building.ec298d7d.jpg"
+  },
+  {
+    "revision": "2e69ccefcfa0140a00fce70fd6a97040",
+    "url": "/PortfolioWebsite/static/media/chair1.2e69ccef.jpg"
+  },
+  {
+    "revision": "1407d56109792adb57f8e8b6732c9a7c",
+    "url": "/PortfolioWebsite/static/media/chair2.1407d561.jpg"
+  },
+  {
+    "revision": "b4fe3e3956f81a0accf2099a9f22c249",
+    "url": "/PortfolioWebsite/static/media/chair3.b4fe3e39.jpg"
+  },
+  {
+    "revision": "c83f2396f6121857bca2b48c606d7d2a",
+    "url": "/PortfolioWebsite/static/media/daniel.c83f2396.jpg"
+  },
+  {
+    "revision": "46a3e8575fccf74c4eeb5adc729d9d62",
+    "url": "/PortfolioWebsite/static/media/danield1.46a3e857.jpg"
+  },
+  {
+    "revision": "cdf7b2e668a60c30c27f75e85f5ebd56",
+    "url": "/PortfolioWebsite/static/media/danield2.cdf7b2e6.jpg"
+  },
+  {
+    "revision": "7e28151de003ff9f99a2634048bd1966",
+    "url": "/PortfolioWebsite/static/media/face.7e28151d.jpg"
+  },
+  {
+    "revision": "27dd143ac1d6f013ff6e4d2c97f35191",
+    "url": "/PortfolioWebsite/static/media/faced.27dd143a.jpg"
+  },
+  {
+    "revision": "39fcc13ac9a77af6235f9e5c8658b05e",
+    "url": "/PortfolioWebsite/static/media/fontd.39fcc13a.jpg"
+  },
+  {
+    "revision": "c743ca73bfa0e3442babc5e28c344304",
+    "url": "/PortfolioWebsite/static/media/fullpiecee.c743ca73.jpg"
+  },
+  {
+    "revision": "73c77cf4747e6a8b398c81cfbb460249",
+    "url": "/PortfolioWebsite/static/media/home1.73c77cf4.jpg"
+  },
+  {
+    "revision": "0f8209f10ecfabb5eea9b431117e5ef3",
+    "url": "/PortfolioWebsite/static/media/jerseyshirt.0f8209f1.jpg"
+  },
+  {
+    "revision": "c1a252722d1793a9611145dc3b2da7a4",
+    "url": "/PortfolioWebsite/static/media/kimono.c1a25272.jpg"
+  },
+  {
+    "revision": "7586927ae3bb0355fbc85eca9bdb3153",
+    "url": "/PortfolioWebsite/static/media/kimonod1.7586927a.jpg"
+  },
+  {
+    "revision": "be124f59b026e096f13f337ae6e6d869",
+    "url": "/PortfolioWebsite/static/media/kimonod2.be124f59.jpg"
+  },
+  {
+    "revision": "15645deaf747bbd89b2342ddf4bf5de8",
+    "url": "/PortfolioWebsite/static/media/kimonod3.15645dea.jpg"
+  },
+  {
+    "revision": "f0fab994aaa06f8a990c9b0431f7e477",
+    "url": "/PortfolioWebsite/static/media/leaf.f0fab994.jpg"
+  },
+  {
+    "revision": "d2a2109bc53de2d9f5a8ee278f32ea7e",
+    "url": "/PortfolioWebsite/static/media/line1.d2a2109b.jpg"
+  },
+  {
+    "revision": "ec4d36ef615045b9742884f87a96250f",
+    "url": "/PortfolioWebsite/static/media/line10.ec4d36ef.jpg"
+  },
+  {
+    "revision": "f8f069c351da9e5e2f7b5c93b2c513d5",
+    "url": "/PortfolioWebsite/static/media/line11.f8f069c3.jpg"
+  },
+  {
+    "revision": "b6c50ef578e1527b122d9fdc1dcffb39",
+    "url": "/PortfolioWebsite/static/media/line12.b6c50ef5.jpg"
+  },
+  {
+    "revision": "c76cdd2f7cf5e08a0bb47e6f068f250a",
+    "url": "/PortfolioWebsite/static/media/line13.c76cdd2f.jpg"
+  },
+  {
+    "revision": "e9471eb49a0a50a7d9a1e9e182b7ae7f",
+    "url": "/PortfolioWebsite/static/media/line14.e9471eb4.jpg"
+  },
+  {
+    "revision": "c66738ff3c7f1469f15dacc79d3e8536",
+    "url": "/PortfolioWebsite/static/media/line15.c66738ff.jpg"
+  },
+  {
+    "revision": "8a9c5c06eec8c8aca9fe6c5bb000aa87",
+    "url": "/PortfolioWebsite/static/media/line16.8a9c5c06.jpg"
+  },
+  {
+    "revision": "88b493b9cd11a8e215fc850f400ec210",
+    "url": "/PortfolioWebsite/static/media/line17.88b493b9.jpg"
+  },
+  {
+    "revision": "094864aa6d17f3289820313f4dad44ed",
+    "url": "/PortfolioWebsite/static/media/line18.094864aa.jpg"
+  },
+  {
+    "revision": "1af0e14cb39f199db5b38d92f0eec990",
+    "url": "/PortfolioWebsite/static/media/line19.1af0e14c.jpg"
+  },
+  {
+    "revision": "154e1cc0d94a25b2f91da9d9a13c8fe3",
+    "url": "/PortfolioWebsite/static/media/line2.154e1cc0.jpg"
+  },
+  {
+    "revision": "079038c4d5ee3e68f96f443f76dc61b9",
+    "url": "/PortfolioWebsite/static/media/line20.079038c4.jpg"
+  },
+  {
+    "revision": "137edaef970a93f399b6efc636670991",
+    "url": "/PortfolioWebsite/static/media/line21.137edaef.jpg"
+  },
+  {
+    "revision": "b261826d1ba7401f62e59d286cff6fee",
+    "url": "/PortfolioWebsite/static/media/line22.b261826d.jpg"
+  },
+  {
+    "revision": "56ba09c6e62260483ab80db5e5b6d0db",
+    "url": "/PortfolioWebsite/static/media/line23.56ba09c6.jpg"
+  },
+  {
+    "revision": "ad012aa519de38f98f452fe69d87d662",
+    "url": "/PortfolioWebsite/static/media/line24.ad012aa5.jpg"
+  },
+  {
+    "revision": "9dc37265e453d5365c6f08790a5f3bd4",
+    "url": "/PortfolioWebsite/static/media/line25.9dc37265.jpg"
+  },
+  {
+    "revision": "2650ca5c2e2b90e8ba11d2f7a457f121",
+    "url": "/PortfolioWebsite/static/media/line26.2650ca5c.jpg"
+  },
+  {
+    "revision": "f75d9d5479158afd6b807c38ffa2da5a",
+    "url": "/PortfolioWebsite/static/media/line27.f75d9d54.jpg"
+  },
+  {
+    "revision": "f8ea1667bd191b157b32008b644574cc",
+    "url": "/PortfolioWebsite/static/media/line28.f8ea1667.jpg"
+  },
+  {
+    "revision": "21835c502ab70872e44469a4545f6f71",
+    "url": "/PortfolioWebsite/static/media/line29.21835c50.jpg"
+  },
+  {
+    "revision": "ea162f0f5d1f70048ff74aeb5ba0643d",
+    "url": "/PortfolioWebsite/static/media/line3.ea162f0f.jpg"
+  },
+  {
+    "revision": "c515f46f758ddb7b4748923eff704e78",
+    "url": "/PortfolioWebsite/static/media/line30.c515f46f.jpg"
+  },
+  {
+    "revision": "a15146a9eb48488b121b2e951e8a3cb1",
+    "url": "/PortfolioWebsite/static/media/line31.a15146a9.jpg"
+  },
+  {
+    "revision": "3019e528bc3b6fd7d88f407d14487ecf",
+    "url": "/PortfolioWebsite/static/media/line32.3019e528.jpg"
+  },
+  {
+    "revision": "7dc0da878f134931cf9d5cec316d6401",
+    "url": "/PortfolioWebsite/static/media/line33.7dc0da87.jpg"
+  },
+  {
+    "revision": "b7c1d6d255088574a454bfae283f4479",
+    "url": "/PortfolioWebsite/static/media/line34.b7c1d6d2.jpg"
+  },
+  {
+    "revision": "e4b216f611c46bf8b70c9e6a6352605d",
+    "url": "/PortfolioWebsite/static/media/line35.e4b216f6.jpg"
+  },
+  {
+    "revision": "bd5da4360f5cf73d1f2146cc5c12563d",
+    "url": "/PortfolioWebsite/static/media/line36.bd5da436.jpg"
+  },
+  {
+    "revision": "5e0dc234c860d508e1e4ed7d7bfc3cea",
+    "url": "/PortfolioWebsite/static/media/line37.5e0dc234.jpg"
+  },
+  {
+    "revision": "bff3a73bfd70bf4b94253d2ec8743976",
+    "url": "/PortfolioWebsite/static/media/line38.bff3a73b.jpg"
+  },
+  {
+    "revision": "e054618f8612ed7e43e28c6d789bee27",
+    "url": "/PortfolioWebsite/static/media/line39.e054618f.jpg"
+  },
+  {
+    "revision": "f3663db25749017f306d7be8b8ae1532",
+    "url": "/PortfolioWebsite/static/media/line4.f3663db2.jpg"
+  },
+  {
+    "revision": "e6acd8d491bf50806ce9dfd0b1adc7cb",
+    "url": "/PortfolioWebsite/static/media/line40.e6acd8d4.jpg"
+  },
+  {
+    "revision": "7d120b944e4877ec74b4f32a621ec3a6",
+    "url": "/PortfolioWebsite/static/media/line41.7d120b94.jpg"
+  },
+  {
+    "revision": "7da8bda846c587057ee38c61998e0b80",
+    "url": "/PortfolioWebsite/static/media/line42.7da8bda8.jpg"
+  },
+  {
+    "revision": "a391ccc72978baf2474d5fb0dd85044e",
+    "url": "/PortfolioWebsite/static/media/line43.a391ccc7.jpg"
+  },
+  {
+    "revision": "9878e07424e039017a77a9092bb9bbd1",
+    "url": "/PortfolioWebsite/static/media/line44.9878e074.jpg"
+  },
+  {
+    "revision": "4b77c74ef9894f47eb451c9676f2db73",
+    "url": "/PortfolioWebsite/static/media/line45.4b77c74e.jpg"
+  },
+  {
+    "revision": "84f5c4f247679850f659c79f308543cf",
+    "url": "/PortfolioWebsite/static/media/line46.84f5c4f2.jpg"
+  },
+  {
+    "revision": "1fc9db53c82ef2b7ee0515cd77175bb2",
+    "url": "/PortfolioWebsite/static/media/line47.1fc9db53.jpg"
+  },
+  {
+    "revision": "75d529eb8f35a86f6c70a9747fcd2ccf",
+    "url": "/PortfolioWebsite/static/media/line48.75d529eb.jpg"
+  },
+  {
+    "revision": "417a0a7abb1cc6b90d9c4b0268c0eb8a",
+    "url": "/PortfolioWebsite/static/media/line49.417a0a7a.jpg"
+  },
+  {
+    "revision": "aef0a4aedbe9e82bc08d261388ca78b5",
+    "url": "/PortfolioWebsite/static/media/line5.aef0a4ae.jpg"
+  },
+  {
+    "revision": "06d8a7603497004d1afea86bc23c4257",
+    "url": "/PortfolioWebsite/static/media/line50.06d8a760.jpg"
+  },
+  {
+    "revision": "35fda4ef3ecd81133bde2cb86f4d1221",
+    "url": "/PortfolioWebsite/static/media/line51.35fda4ef.jpg"
+  },
+  {
+    "revision": "851ae234c03756f90bd8aeca9a86884e",
+    "url": "/PortfolioWebsite/static/media/line52.851ae234.jpg"
+  },
+  {
+    "revision": "79b3d4dfa11e046bb70c303cea2eb728",
+    "url": "/PortfolioWebsite/static/media/line53.79b3d4df.jpg"
+  },
+  {
+    "revision": "498a1416963086b92072a9e13e18f619",
+    "url": "/PortfolioWebsite/static/media/line54.498a1416.jpg"
+  },
+  {
+    "revision": "1f257e158cf72b56ae92cb58a609f5f6",
+    "url": "/PortfolioWebsite/static/media/line55.1f257e15.jpg"
+  },
+  {
+    "revision": "5c12a2bd42ae450f81051f1dab0495ec",
+    "url": "/PortfolioWebsite/static/media/line56.5c12a2bd.jpg"
+  },
+  {
+    "revision": "f4e41e3d38d4f8452f69324c3bb07b4c",
+    "url": "/PortfolioWebsite/static/media/line57.f4e41e3d.jpg"
+  },
+  {
+    "revision": "7e7de523b00b7f08054471dd209e38c2",
+    "url": "/PortfolioWebsite/static/media/line58.7e7de523.jpg"
+  },
+  {
+    "revision": "181c2ac76e16311a08111189c8d6ca68",
+    "url": "/PortfolioWebsite/static/media/line59.181c2ac7.jpg"
+  },
+  {
+    "revision": "9f50bf6c811183f9e254b99ecc4b807d",
+    "url": "/PortfolioWebsite/static/media/line6.9f50bf6c.jpg"
+  },
+  {
+    "revision": "f4d14cb48861b70bbc4a347348cd6acb",
+    "url": "/PortfolioWebsite/static/media/line60.f4d14cb4.jpg"
+  },
+  {
+    "revision": "1fb9bbc5eeb2b43530abbae89d2825b6",
+    "url": "/PortfolioWebsite/static/media/line7.1fb9bbc5.jpg"
+  },
+  {
+    "revision": "ed727d2160ce23baae7f8e25424ee050",
+    "url": "/PortfolioWebsite/static/media/line8.ed727d21.jpg"
+  },
+  {
+    "revision": "1dce685f77fa50b6df3542e5903ab2d4",
+    "url": "/PortfolioWebsite/static/media/line9.1dce685f.jpg"
+  },
+  {
+    "revision": "924d6b343af13ba341d2a93e918a9d12",
+    "url": "/PortfolioWebsite/static/media/long.924d6b34.jpg"
+  },
+  {
+    "revision": "8bd110faf23c2ee543d958d28da5aaed",
+    "url": "/PortfolioWebsite/static/media/longd2.8bd110fa.jpg"
+  },
+  {
+    "revision": "df9b6bba9712081c038a3b2fa887f77a",
+    "url": "/PortfolioWebsite/static/media/longd3.df9b6bba.jpg"
+  },
+  {
+    "revision": "2d812fcbcc6a5f37f11fafda541876d0",
+    "url": "/PortfolioWebsite/static/media/longd4.2d812fcb.jpg"
+  },
+  {
+    "revision": "555b71b5690b06203c3b205f87920fc9",
+    "url": "/PortfolioWebsite/static/media/manga.555b71b5.jpg"
+  },
+  {
+    "revision": "57a4e72af9a2b5a636963cf25b3e0f5b",
+    "url": "/PortfolioWebsite/static/media/rin.57a4e72a.jpg"
+  },
+  {
+    "revision": "5e9a99209f55aad0e254826d1712a7b6",
+    "url": "/PortfolioWebsite/static/media/rind3.5e9a9920.jpg"
+  },
+  {
+    "revision": "ed25d1141b54047ddc971b8775afe128",
+    "url": "/PortfolioWebsite/static/media/rind4.ed25d114.jpg"
+  },
+  {
+    "revision": "7c8191615ee34fdb2b2a0df8df0288bb",
+    "url": "/PortfolioWebsite/static/media/sky.7c819161.jpg"
+  },
+  {
+    "revision": "1b0af06b7f94803ee8f6b9f9f356c31b",
+    "url": "/PortfolioWebsite/static/media/squid.1b0af06b.jpg"
+  },
+  {
+    "revision": "2bfa70b6d4c338996c2de58f944b4d95",
+    "url": "/PortfolioWebsite/static/media/squidd1.2bfa70b6.jpg"
+  },
+  {
+    "revision": "7b033799e9a5a748421229a7c470a3fc",
+    "url": "/PortfolioWebsite/static/media/squidd3.7b033799.jpg"
+  },
+  {
+    "revision": "595fb6639012176b9291488f066c4765",
+    "url": "/PortfolioWebsite/static/media/squidd4.595fb663.jpg"
+  }
+]);
